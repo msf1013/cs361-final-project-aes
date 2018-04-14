c95a900f3f6da87a5d2aabfd9a17ad7b262d1094
Mario Sergio Fuentes <msf1013@gmail.com>
2018-04-14 00:54:54 -0500

    Initial key expansion code.

--- aes.py
+++ aes.py
@@ -1,5 +1,6 @@
 # AES algorithm
 import sys
+from common import expand_key
 from encrypt import encrypt
 from decrypt import decrypt
 
@@ -17,18 +18,31 @@ def get_options(argv):
 options = get_options(sys.argv)
 
 # --keysize: Either 128 or 256 bits
-key_size = int(options['--keysize'])
+key_size         = int(options['--keysize'])
 # --keyfile: Name of file containing the key
-key_file= options['--keyfile']
+key_file_name    = options['--keyfile']
 # --inputfile: Name of file containing plaintext/ciphertext to process
-input_file = options['--inputfile']
+input_file_name  = options['--inputfile']
 # --outputfile: Name of file containing the resulting plaintext/ciphertext
-output_file = options['--outputfile']
+output_file_name = options['--outputfile']
 # --mode: Either 'encrypt' or 'decrypt'
-mode = options['--mode']
+mode             = options['--mode']
 
 # Call encryption or decryption algorithm accordingly
+#input_file  = open(input_file_name, "rb")
+#key_file    = open(key_file_name, "rb")
+#output_file = open(output_file_name, "wb")
+
+#input_bytes = input_file.read()
+#key_bytes = key_file.read()
+key_bytes = bytearray.fromhex("2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c")
+key_size = 128
+expanded_key = expand_key(key_bytes, key_size) # TODO: Key expansion
+print(len(expanded_key))
+print(expanded_key)
+
 if mode == 'encrypt':
-    encrypt()
+    output_bytes = encrypt(input_bytes, expanded_key, key_size)
+    output_file.write(output_bytes)
 else:
     decrypt()
--- encrypt.py
+++ encrypt.py
@@ -1,2 +1,9 @@
-def encrypt():
-    print("encrypted")
+def encrypt(input_bytes, expanded_key, key_size):
+    # Add padding if necessary
+    if len(input_bytes) % 16 != 0:
+        input_bytes.extend([b'\0'] * (16 - (len(input_bytes) % 16)))
+    # Encrypt each block of input plaintext
+    output_bytes = []
+    for i in range(0, len(input_bytes), 16):
+        output_bytes += cipher(input_bytes[i:i+16], expanded_key)
+    return output_bytes
